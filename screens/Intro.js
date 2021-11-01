8244ccfa1f7e531db53fb646a93555a3ff82cd10
Rudi <rudix.lab@gmail.com>
2021-11-01 15:58:40 +0200

    ddd

--- screens/Intro.js
+++ screens/Intro.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+
 import AppIntroSlider from 'react-native-app-intro-slider';
 
 const slides = [
@@ -7,32 +7,30 @@ const slides = [
     key: 1,
     title: 'Title 1',
     text: 'Description.\nSay something cool',
-    image: require('./assets/1.jpg'),
+    image: require('./assets/intro/1.jpg'),
     backgroundColor: '#59b2ab',
   },
   {
     key: 2,
     title: 'Title 2',
     text: 'Other cool stuff',
-    image: require('./assets/2.jpg'),
+    image: require('./assets/intro/2.jpg'),
     backgroundColor: '#febe29',
   },
   {
     key: 3,
     title: 'Rocket guy',
-    text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
-    image: require('./assets/3.jpg'),
+    text: "I'm already out of descriptions\n\nLorem ipsum bla bla bla",
+    image: require('./assets/intro/3.jpg'),
     backgroundColor: '#22bcb5',
-  }
+  },
 ];
 
 const Intro = () => {
-  function renderItem() {
-  }
-  function onDone() {
-    
-  }
-  return <AppIntroSlider renderItem={renderItem} data={slides} onDone={onDone}/>;
-  
-}
-
+  function renderItem () {}
+  function onDone () {}
+  return (
+    <AppIntroSlider renderItem={renderItem} data={slides} onDone={onDone} />
+  );
+};
+export default Intro;
